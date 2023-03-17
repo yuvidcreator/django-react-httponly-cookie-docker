8db3dd624571264dfb727b8fcffb27d232b7abb6
Yuvraaj Limbole <yuvrajl.103@gmail.com>
2023-03-18 03:01:34 +0530

    API Url bug fixed

--- client/src/axios/index.js
+++ client/src/axios/index.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const API_URL = "http://localhost:8080/api/v1"
+const API_URL = `${process.env.BACKEND_API_URL}/api/v1`
 
 export const axiosInstance = axios.create({
     baseURL: API_URL,
