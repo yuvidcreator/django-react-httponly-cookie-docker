6692c7be8b375676e9cd60a6eb1b6488bcebf9f4
Yuvraaj Limbole <yuvrajl.103@gmail.com>
2023-03-18 03:12:05 +0530

    API Url bug fixed

--- client/src/axios/index.js
+++ client/src/axios/index.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const API_URL = `${process.env.BACKEND_API_URL}/api/v1`
+const API_URL = `${process.env.REACT_APP_API_URL}/api/v1`
 
 export const axiosInstance = axios.create({
     baseURL: API_URL,
